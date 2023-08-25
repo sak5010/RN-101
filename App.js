418b2832ff0f57383c00cec5dddf87e033c9efa2
sak5010 <sak5010@naver.com>
2023-08-25 15:42:40 +0900

    dev env test: 로그인, 실시간 수정사항 반영 확인

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is Text component area!</Text>
+      <Text>This is Text component area in RN!</Text>
       <StatusBar style="auto" />
     </View>
   );
